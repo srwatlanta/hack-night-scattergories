e4ab5272e3e176b94e06da00e0a8468cc42d2458
Amanda Treutler <amandathedev@gmail.com>
2019-07-10 18:38:29 -0500

    gems

--- game.rb
+++ game.rb
@@ -1,26 +1,27 @@
+#!/usr/bin/ruby
+# require "rubygems"
+require "json"
+require "pp"
+
+json = File.read("./data/categories.json")
+obj = JSON.parse(json)
+
+pp obj
+
+# puts "hello"
+
 class Game
+  alphabet_array = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"]
 
-    alphabet_array = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L"]
+  def list
+  end
 
-    def list
-    end
-
-    def category
-    end
-
-
-
-
-
-
-
-
-
-
+  def category
+  end
 end
 
-generate a list from the json file sampling 12 options
-generate a letter from an array of 20 letters
-create a 3  minute timer
-provide input field for answers
-how to validate answers and award points for them
+# generate a list from the json file sampling 12 options
+# generate a letter from an array of 20 letters
+# create a 3  minute timer
+# provide input field for answers
+# how to validate answers and award points for them
